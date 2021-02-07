c598c14268f28843f9a3c8f28c0e5ee4c9fb209b
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2021-02-07 15:26:38 +0300

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// one player at a time
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
